33ae783f9012196a189b1f29362ff0892383180d
renatateixeiratimoteo <68713798+renatateixeiratimoteo@users.noreply.github.com>
2020-07-23 22:25:07 -0300

    Add files via upload

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Susep.Domain.Commands;
+using Susep.Domain.Contracts.Repositories;
+using Susep.Domain.Entities;
+using Susep.Domain.Handler;
+
+namespace Susep.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [Route("")]
+        [HttpPost]
+        public CommandResult Create([FromBody] ClienteCadastroCommand command, [FromServices] ClienteHandler handler)
+        {
+            return (CommandResult)handler.Handle(command);
+        }
+
+
+        [Route("")]
+        [HttpPut]
+        public CommandResult Update([FromBody] ClienteEdicaoCommand command, [FromServices] ClienteHandler handler)
+        {
+            return (CommandResult)handler.Handle(command);
+        }
+
+
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<Cliente> Get([FromServices] IClienteRepository repository)
+        {
+            return repository.FindAll();
+        }
+
+        [Route("")]
+        [HttpDelete]
+        public CommandResult Delete([FromBody] ClienteExclusaoCommand command, [FromServices] ClienteHandler handler)
+        {
+            return (CommandResult)handler.Handle(command);
+        }
+    }
+}
